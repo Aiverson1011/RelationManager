09e5fbb311d938b8543f62ad1adb9ca7ccdeb468
Amanda Iverson <amanda@codefellows.com>
2018-08-14 23:45:06 -0700

    Update SQLExec.cpp

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -1,7 +1,7 @@
 /**
  * @file SQLExec.cpp - implementation of SQLExec class
- * @author Kevin Lundeen
- * @student Wonseok Seo, Amanda Iverson
+ * @Professor: Kevin Lundeen
+ * @Students: Wonseok Seo, Amanda Iverson
  * @see "Seattle University, CPSC5300, Summer 2018"
  */
 #include <algorithm>
