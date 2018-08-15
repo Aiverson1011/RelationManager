b178429f512e061922b2b107d8f9e569af3298c5
Amanda Iverson <amanda@codefellows.com>
2018-08-14 23:45:57 -0700

    Update heap_storage.h

--- heap_storage.h
+++ heap_storage.h
@@ -5,8 +5,8 @@
  * HeapFile: DbFile
  * HeapTable: DbRelation
  *
- * @author Kevin Lundeen
- * @student Wonseok Seo, Amanda Iverson
+ * @Professor: Kevin Lundeen
+ * @Students: Wonseok Seo, Amanda Iverson
  * @see "Seattle University, CPSC5300, Summer 2018"
  * Note: Slight modification for style and additional implementation for sprint3
  */
